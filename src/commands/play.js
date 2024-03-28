d4c313ee22388cf6ea793ba199c8121f341585ab
Johannes (Jotrorox) Müller <jotrorox@gmail.com>
2024-03-28 21:59:18 +0100

    First working version

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,15 +1,48 @@
 const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
+const fs = require('node:fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('Joins your channel and plays sum LoFi!'),
+        .setDescription('Joins your channel and plays sum LoFi!'),
 	async execute(interaction) {
-		const embed = new EmbedBuilder()
-			.setTitle("Not Implemented!")
-			.setDescription("This feature has not been implemented yet!\nBut @Jotrorox is working hard on it!");
+        if (!interaction.member.voice) {
+            const embed = new EmbedBuilder()
+			    .setTitle("Error!")
+			    .setDescription("You have to be in a vc to use this command!")
+                .setColor(Colors.Red);
+                await interaction.reply({ embeds: [embed], ephemeral: true });
+                return;
+        }
+    
+        const connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+    
+        const player = createAudioPlayer();
+        connection.subscribe(player);
+    
+        const resource = createAudioResource(fs.createReadStream('./rsc/empty-mind-118973.mp3'), {
+            inputType: StreamType.Arbitrary,
+        });
+    
+        player.play(resource);
+    
+        player.on('error', error => {
+            console.error('Error:', error);
+        });
+    
+        player.on('idle', () => {
+            connection.destroy();
+        });
+    
+        const embed = new EmbedBuilder()
+			.setTitle("Playing!")
+			.setDescription("The bot successfully joined your vc and should've started playing LoFi. If that is not the case please contact @jotrorox.");
 
-		// Reply with the embed and set the response to be ephemeral
-		await interaction.reply({ embeds: [embed], ephemeral: true });
+        await interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits, Events, Collection, REST, Routes } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 const fs = require('node:fs');
 const path = require('node:path');
 const dotenv = require('dotenv');
@@ -37,37 +36,6 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on(Events.MessageCreate, async message => {
     return;
-
-    if (message.author.bot) return;
-
-    if (!message.member.voice.channel) {
-        return message.reply('You must be in a voice channel to play music!');
-    }
-
-    const connection = joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
-        guildId: message.guild.id,
-        adapterCreator: message.guild.voiceAdapterCreator,
-    });
-
-    const player = createAudioPlayer();
-    connection.subscribe(player);
-
-    const resource = createAudioResource(fs.createReadStream('./rsc/empty-mind-118973.mp3'), {
-        inputType: StreamType.Arbitrary,
-    });
-
-    player.play(resource);
-
-    player.on('error', error => {
-        console.error('Error:', error);
-    });
-
-    player.on('idle', () => {
-        connection.destroy();
-    });
-
-    message.reply('Now playing!');
 });
 
 client.on(Events.InteractionCreate, async interaction => {
