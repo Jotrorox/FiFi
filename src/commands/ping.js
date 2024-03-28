cebab510964e7c47f9fed81dbfc5f1924101f918
Johannes (Jotrorox) Müller <jotrorox@gmail.com>
2024-03-28 19:44:38 +0100

    Adding Slash Comamnds and a ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		// Calculate the bot's ping
+		const ping = Date.now() - interaction.createdTimestamp;
+
+		// Determine the color of the embed based on the ping value
+		let color = ping < 200 ? Colors.Green : ping < 500 ? Colors.Yellow : Colors.Red;
+
+		const embed = new EmbedBuilder()
+			.setTitle("Pong!")
+			.setDescription(`Bot's ping: ${ping}ms`)
+			.setColor(color);
+
+		// Reply with the embed and set the response to be ephemeral
+		await interaction.reply({ embeds: [embed], ephemeral: true });
+	},
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+const { Client, GatewayIntentBits, Events, Collection, REST, Routes } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
-const fs = require('fs');
+const fs = require('node:fs');
+const path = require('node:path');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -13,11 +14,30 @@ const client = new Client({
     ]
 });
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+
+const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(foldersPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+	}
+
 client.once(Events.ClientReady, readyClient => {
     console.log(`The bot is now ready and logged in as ${readyClient.user.username}`);
+    registerCommands();
 });
 
 client.on(Events.MessageCreate, async message => {
+    return;
+
     if (message.author.bot) return;
 
     if (!message.member.voice.channel) {
@@ -50,4 +70,66 @@ client.on(Events.MessageCreate, async message => {
     message.reply('Now playing!');
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
 client.login(process.env.DISCORD_TOKEN);
+
+function registerCommands() {
+    const commands = [];
+    // Grab all the command folders from the commands directory you created earlier
+    const foldersPath = path.join(__dirname, 'commands');
+    
+    const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(foldersPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+    
+    
+    // Construct and prepare an instance of the REST module
+    const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+    
+    // and deploy your commands!
+    (async () => {
+        try {
+            console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    
+            // The put method is used to fully refresh all commands in the guild with the current set
+            const data = await rest.put(
+                Routes.applicationCommands(process.env.CLIENT_ID),
+                { body: commands },
+            );
+    
+            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        } catch (error) {
+            // And of course, make sure you catch and log any errors!
+            console.error(error);
+        }
+    })();
+}
