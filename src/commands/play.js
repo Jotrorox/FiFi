edf1cf263b8b18982feaeac485b199fc51ec5b68
Johannes (Jotrorox) Müller <jotrorox@gmail.com>
2024-03-29 20:51:41 +0100

    Add streaming from Youtube

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,12 +1,15 @@
 const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 const fs = require('node:fs');
+const ytdl = require('ytdl-core');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
         .setDescription('Joins your channel and plays sum LoFi!'),
 	async execute(interaction) {
+        const youtubeURL = 'https://www.youtube.com/watch?v=FE3Imqn63sc';
+
         if (!interaction.member.voice) {
             const embed = new EmbedBuilder()
 			    .setTitle("Error!")
@@ -22,53 +25,23 @@ module.exports = {
             adapterCreator: interaction.guild.voiceAdapterCreator,
         });
     
+        const stream = ytdl(youtubeURL, { filter: 'audioonly', quality: 'highestaudio' });
+        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
         const player = createAudioPlayer();
+
+        player.play(resource);
         connection.subscribe(player);
-
-        const directoryPath = './rsc/';
-        const music_files = [];
-        
-        fs.readdir(directoryPath, (err, files) => {
-            if (err) {
-                console.error('Error reading directory:', err);
-                return;
-            }
-            
-            music_files.push(...files);
-            const music_path = directoryPath + music_files[Math.floor(Math.random() * music_files.length)];
-
-            const resource = createAudioResource(fs.createReadStream(music_path), {
-                inputType: StreamType.Arbitrary,
-            });
-        
-            player.play(resource);
-        });
 
         player.on('error', error => {
             console.error('Error:', error);
         });
     
         player.on('idle', () => {
-            const directoryPath = './rsc/';
-            const music_files = [];
-            
-            fs.readdir(directoryPath, (err, files) => {
-                if (err) {
-                    console.error('Error reading directory:', err);
-                    return;
-                }
-                
-                music_files.push(...files);
-                const music_path = directoryPath + music_files[Math.floor(Math.random() * music_files.length)];
-
-                const resource = createAudioResource(fs.createReadStream(music_path), {
-                    inputType: StreamType.Arbitrary,
-                });
-            
-                player.play(resource);
-            });
+            const stream = ytdl(youtubeURL, { filter: 'audioonly', quality: 'highestaudio' });
+            const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
+            player.play(resource);
         });
-    
+        
         const embed = new EmbedBuilder()
             .setColor(Colors.Purple)
 			.setTitle("Playing!")
